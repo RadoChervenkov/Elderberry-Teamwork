821d044ef6691c73a415652af20e1a99e99d486d
Viktor90 <viktor102690@gmail.com>
2014-09-18 12:14:57 +0300

    Starting project + code first

---
+++ ElderberryLottery/ElderberryLottery.Models/GameCode.cs
@@ -0,0 +1,24 @@
+﻿namespace ElderberryLottery.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GameCode
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(10)]
+        public string Value { get; set; }
+
+        [Required]
+        public bool IsWinning { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ ElderberryLottery/ElderberryLotttery.Data/ElderberryDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace ElderberryLotttery.Data
+{
+    using ElderberryLottery.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data.Entity;
+    using ElderberryLotttery.Data.Migrations;
+
+    public class ElderberryDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ElderberryDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ElderberryDbContext, Configuration>());
+            //Database.SetInitializer<ElderberryDbContext>(null);
+        }
+
+        public static ElderberryDbContext Create()
+        {
+            return new ElderberryDbContext();
+        }
+
+        IDbSet<GameCode> GameCodes { get; set; }
+    }
+}
