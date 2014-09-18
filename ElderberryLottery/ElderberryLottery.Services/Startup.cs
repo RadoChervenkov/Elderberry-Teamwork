e3624b06abd772ffbb47b9118a2ae8b2866882bd
Ani_Mihaylova <ani.ad.lib@gmail.com>
2014-09-18 13:05:23 +0300

    Add ElderberryData and Ninject

--- ElderberryLottery/ElderberryLottery.Services/Startup.cs
+++ ElderberryLottery/ElderberryLottery.Services/Startup.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
+using Ninject.Web.WebApi;
+using Ninject.Web;
+using Ninject;
+using System.Reflection;
+using ElderberryLotttery.Data;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(ElderberryLottery.Services.Startup))]
 
@@ -13,6 +21,22 @@ namespace ElderberryLottery.Services
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(GlobalConfiguration.Configuration);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            RegisterMappings(kernel);
+            return kernel;
+        }
+
+        private static void RegisterMappings(StandardKernel kernel)
+        {
+            kernel.Bind<IElderberryLotteryData>().To<ElderberryLotteryData>()
+                .WithConstructorArgument("context",
+                    c => new ElderberryDbContext());          
         }
     }
 }
