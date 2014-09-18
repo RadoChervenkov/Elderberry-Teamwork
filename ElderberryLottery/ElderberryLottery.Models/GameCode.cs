150f37e31a905dde0f4670a39a1331bc9b39b1c2
Ani_Mihaylova <ani.ad.lib@gmail.com>
2014-09-18 16:30:14 +0300

    Add requests

--- ElderberryLottery/ElderberryLottery.Models/GameCode.cs
+++ ElderberryLottery/ElderberryLottery.Models/GameCode.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -12,13 +13,14 @@
         public int Id { get; set; }
 
         [Required]
+        [Index(IsUnique=true)]
         [MinLength(10)]
         [MaxLength(10)]
         public string Value { get; set; }
 
-        [Required]
         public bool IsWinning { get; set; }
 
-        public virtual ApplicationUser User { get; set; }
+
+       // public virtual ApplicationUser User { get; set; }
     }
 }
---
+++ ElderberryLottery/ElderberryLottery.Services/Controllers/GameCodeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using ElderberryLottery.Models;
+using ElderberryLotttery.Data;
+using Microsoft.AspNet.Identity;
+using System.Security.Principal;
+using ElderberryLottery.Services.Models;
+
+namespace ElderberryLottery.Services.Controllers
+{
+    [Authorize]
+    public class GameCodeController : ApiController
+    {
+        private IElderberryLotteryData data;
+
+        public GameCodeController(IElderberryLotteryData data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetUserCodes()
+        {
+            var userId = this.GetUserId();
+
+            var currentUser = this.data.Users.All().FirstOrDefault(x => x.Id == userId);
+
+            var userCodes = currentUser.GameCodes;
+
+            return Ok(userCodes);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(GameCodeModel model)
+        {
+            var userId = this.GetUserId();
+
+            var existingCode = this.data.GameCodes.All().FirstOrDefault(x => x.Value == model.Value);
+
+            if (existingCode == null)
+            {
+                return NotFound();
+            }
+
+            var existingUser = this.data.Users.All().FirstOrDefault(user => user.Id == userId);
+
+            existingUser.GameCodes.Add(existingCode);
+
+            this.data.SaveChanges();
+
+            if (existingCode.IsWinning)
+            {
+                return Ok("You win!");
+            }
+
+            return Ok("You did not win!");
+        }
+
+        private string GetUserId()
+        {
+            return Thread.CurrentPrincipal.Identity.GetUserId();
+        }
+
+    }
+}
